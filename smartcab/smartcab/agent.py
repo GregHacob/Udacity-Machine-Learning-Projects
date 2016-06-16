bae482cccc443fbda52f4aa8c7a101f0c6e8d3f6
Greg Hacobian <greg.hacobian@gmail.com>
2016-06-16 08:38:55 -0700

    Implemented Q-Learning
    First submission - Added qlearning class to the smarcab

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -2,6 +2,26 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+from qlearning import QLearning
+import sys
+
+class BasicAgent(Agent):
+    def __init__(self, env):
+        super(BasicAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
+        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+        self.color = 'red'  # override color
+
+    def reset(self, destination=None):
+        self.planner.route_to(destination)  
+        
+    def update(self, t):
+        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
+        inputs = self.env.sense(self)
+        deadline = self.env.get_deadline(self)
+        action = random.choice(Environment.valid_actions[1:])
+        reward = self.env.act(self, action)
+        print "Basic.update(): next move = {}".format(action)  # [debug]
+
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -10,43 +30,111 @@ class LearningAgent(Agent):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        # TODO: Initialize any additional variables here
+        
+        self.cumulative_reward = 0
+ 
+        self.ai = QLearning()        
+          
 
     def reset(self, destination=None):
-        self.planner.route_to(destination)
-        # TODO: Prepare for a new trip; reset any variables here, if required
+        self.planner.route_to(destination) 
+        self.cumulative_reward = 0        
 
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-
-        # TODO: Update state
         
-        # TODO: Select action according to your policy
-        action = None
+        light = inputs['light']
+        oncoming = inputs['oncoming']
+        left = inputs['left']
+        right = inputs['right']
+       
+        #Update the current observed state
+        self.state = (light, oncoming, left, right, self.next_waypoint)
+        current_state = self.state
+        
+        #Choose an action
+        action = self.ai.chooseAction(current_state, self.env.valid_actions, True)
 
-        # Execute action and get reward
+        #Execute action and get reward
         reward = self.env.act(self, action)
-
-        # TODO: Learn policy based on state, action, reward
+        self.cumulative_reward += reward
+        
+        #Update the state variables after action  and move to a new state             
+        new_inputs = self.env.sense(self)
+        self.next_waypoint = self.planner.next_waypoint()
+        light = new_inputs['light']
+        oncoming = new_inputs['oncoming']
+        left = new_inputs['left']
+        right = new_inputs['right']
+        
+        state_prime = (light, oncoming, left, right, self.next_waypoint)
+          
+        #Get the best action for the new state
+        action_prime = self.ai.chooseAction(state_prime, self.env.valid_actions, False)    
+        
+        #Learn policy based on state, action, reward
+        self.ai.learn(current_state, action, state_prime, action_prime, reward )
+       
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+    
+    def getStatetuple(self, state):
+        light = state['light']
+        oncoming = state['oncoming']
+        left = state['left']
+        right = state['right']
+        return (light, oncoming, left, right, self.next_waypoint)
+     
+    
+    def SaveQTable(self):
+       import csv
+       out_filename = 'q_table.csv'
+       f = open(out_filename, 'wb')
+       writer = csv.writer(f)
+       for state, action_function in self.ai.getQfunction().items():
+           q_row = []
+           q_row.append(state)
+           for action in self.env.valid_actions:
+               q_row.append(action_function[action])
+           writer.writerow(q_row)
+       f.close()
+       print 'Written to file: ' + out_filename
+ 
+    def initQFunction(self, filename):
+        import pandas as pd
+        q_df = pd.read_csv(filename, sep=',', header=None)
 
-
-def run():
+        try:
+            for row in xrange(q_df.shape[0]):           
+                state = q_df.ix[row][0] 
+                action_function = {}
+                col = 1
+                for action in ['Up','down','none','forward']:
+                    action_function[action] = q_df.ix[row][col]
+                    col += 1
+                self.ai.Q_function[state] = action_function
+        except IOError: 
+            print 'There is no file named', filename  
+     
+def run(argv):
     """Run the agent for a finite number of trials."""
-
+    
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
-    a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
+    a = e.create_agent(LearningAgent)  # create Smart agent
+    #a = e.create_agent(BasicAgent)  # create Basic agent
+    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
+    if (len(argv) == 2):
+     a.initQFunction(argv[1])
     # Now simulate it
-    sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=10)  # press Esc or close pygame window to quit
-
+    sim = Simulator(e, update_delay=.1)  # reduce update_delay to speed up simulation
+    sim.run(n_trials=100)  # press Esc or close pygame window to quit
+    if (len(argv) < 2):
+     a.SaveQTable()
 
 if __name__ == '__main__':
-    run()
+    run(sys.argv)
---
+++ smartcab/smartcab/qlearning.py
@@ -0,0 +1,59 @@
+import random
+
+class QLearning:
+    def __init__(self, init_value = 0, epsilon=0.2, alpha=0.2, gamma=0.9):
+    
+        # Initialize the Q-function as a dictionary (state) of dictionaries (actions)
+        self.Q_function = {}
+        
+        # Initial value of any (state, action) tuple is an arbitrary random number
+        self.init_value = init_value
+        
+        ## Discount factor gamma
+        self.gamma = gamma
+        
+        ## Learning rate alpha: 0 (no learning) vs 1 (consider only most recent information)
+        self.alpha = alpha
+        
+        ## Parameter of the epsilon-greedy action selection strategy
+        self.epsilon = epsilon
+
+    def getQvalue(self, state, action):
+        return self.Q_function[state][action]
+    
+    def getQfunction(self):
+        return self.Q_function    
+
+    def chooseAction(self, state, actions, is_current):
+ 
+        if state in self.Q_function:
+            ## Find the action that has the highest value
+            action_function = self.Q_function[state]
+            q_action = max(action_function, key = action_function.get)
+            if is_current:
+                ## Generate a random action
+                rand_action = random.choice(actions)
+                ## Select action using epsilon-greedy heuristic
+                rand_num = random.random()
+                action = q_action if (rand_num <= self.epsilon) else rand_action
+            else:
+                action = q_action
+        else:
+            ## Initialize <state, action> pairs and select random action
+            action_function = {}
+            for action in actions:
+                action_function[action] = self.init_value
+            self.Q_function[state] = action_function
+            action = random.choice(actions)        
+        
+        return action
+        
+    def learn(self, state1, action1, state2, action2, reward):
+         ## Get the new Q_value
+        current_q_value = self.getQvalue(state1,action1)
+        new_state_q_value = self.getQvalue(state2,action2)
+        
+        ## Update the Q-function
+        #self.Q_function[state1][action1] = (1 - self.alpha) * current_q_value + self.alpha * (reward + self.gamma * new_state_q_value)
+        self.Q_function[state1][action1] = current_q_value + self.alpha*(reward + self.gamma * new_state_q_value - current_q_value)
+
